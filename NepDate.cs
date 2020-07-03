80159cae5b1aac073da9f24d12bcf2e6d44e1321
acp-anjan <paudelanjanchandra@gmail.com>
2020-07-03 19:38:51 +0545

    @anjan initial commit

---
+++ NepDate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nepdate
+{
+    public class NepDate
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        private string _weekDayName;
+
+     
+        public string WeekDayName
+        {
+            get { return _weekDayName; }
+            set { _weekDayName = value; }
+        }
+
+        public string MonthName { get; set; }
+        public int WeekDay { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}/{2}", Year, Month, Day);
+        }
+        public string ToLongDateString()
+        {
+            return string.Format("{0}, {1} {2}, {3}", WeekDayName, MonthName, Day, Year);
+        }
+
+        public string ToLongsDateString()
+        {
+            return string.Format("{0} {1}, {2}",  MonthName, Day, Year);
+        }
+
+        public void setNepDate(int yr, int mth, int day)
+        {
+            Year = yr;
+            Month = mth;
+            Day = day;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace nepdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NepDate daate = new NepDate();
+            daate.setNepDate(2075, 05, 25);
+
+            NepDateConverter conv = new NepDateConverter();
+            DateTime  a = NepDateConverter.NepToEng(2076, 5, 30);
+            Console.WriteLine("DateConversion:::::");
+            Console.WriteLine(a);
+
+            var b = NepDateConverter.EngToNep(2019, 7, 16);
+            b.ToLongDateString();
+            Console.WriteLine("DateConversion:::::");
+            Console.WriteLine(b.ToLongsDateString());
+
+            Console.WriteLine(b.Year);
+            Console.WriteLine(b.Month);
+            Console.WriteLine(b.Day);
+            
+
+            //if (b.Month < 4)
+            //{
+            //    Console.WriteLine("FisalYear :"+(b.Year - 1)+"/"+ b.Year);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("FisalYear :" + (b.Year) + "/" + (b.Year+1));
+            //}
+
+        }
+    }
+}
